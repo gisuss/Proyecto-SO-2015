b4bc360b4551c1c1279ea35ddd3c00da07f65f72
Jesus <gisuss8520@gmail.com>
2015-04-12 15:45:38 -0430

    Update centro.c

--- Escritorio/centro.c
+++ Escritorio/centro.c
@@ -26,30 +26,35 @@
 
 /****************  VARIABLES Y ESTRUCTURAS GLOBALES **************/
 
-#ifndef N
+#ifndef N																/** Cantidad de Thread de Usuarios **/
 #define N 300
 #endif
 
-#ifndef M
-#define M 15
+#ifndef M																/** Cantidad de Impresoras **/
+#define M 7
+#endif
+
+#ifndef MAX																/** Variable referencial **/
+#define MAX 10000
 #endif
 
 typedef enum {false, true} bool;
 
 typedef struct {
-	int tinta;
-	int identificador;
-	sem_t ocupada;
+	int tinta;															/** Indica nivel de Tinta **/
+	int identificador;													/** Identificador de Impresora **/
+	int uso;															/** Cantidad de veces que se ha usado la Impresora **/
+	sem_t ocupada;														/** Semaforo que indica si esta libre o no la Impresora **/
 }Impresora;
 
-Impresora Centro[M];
-FILE *output;
-int marca, Impresoras_libres;
-pthread_mutex_t mutex_tecnico = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t mutex_requerido = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t cond_impresoras_libres = PTHREAD_COND_INITIALIZER;
-sem_t mutex_impresoras_libres;
-sem_t mutex_Centro;
+Impresora Centro[M];													/** Arreglo de Impresoras. Representa un Recurso Critico **/
+FILE *output;															/** Archivo de Salida **/
+int marca, Impresoras_libres; 											/** Recurso Critico "Impresoras_libres" **/
+pthread_mutex_t mutex_tecnico = PTHREAD_MUTEX_INITIALIZER;				/** Semaforo Binario, Tecnico **/
+pthread_mutex_t mutex_requerido = PTHREAD_MUTEX_INITIALIZER;			/** Garantiza EM dentro de requerir_impresora **/
+pthread_cond_t cond_impresoras_libres = PTHREAD_COND_INITIALIZER;		/** Semaforo Condicion que duerme miestras no hayan Impresoras disponibles **/
+sem_t mutex_impresoras_libres;											/** Semaforo que garantiza la consistencia de datos **/
+sem_t mutex_Centro;														/** Garantiza la EM dentro del arreglo Centro **/
 
 /************  DECLARACION DE PROCEDIMIENTOS **************/
 
@@ -65,11 +70,11 @@ int main(void) {
 	
 	/************  DECLARACION DE VARIABLES **************/	
 	
-	pthread_t Usuarios[N]; 	 											/** Vector de threads de Usuarios de tamaño N **/
+	pthread_t Usuarios[N]; 	 											/** Arreglo de threads de Usuarios de tamaño N **/
 	pthread_t Intro;  													/** Thread que lanza la presentacion de bienvenida **/
 	pthread_attr_t atributos;  											/** Representa el cuerpo de cada Thread **/
 	struct sched_param fifo_param;  									/** "sched_param" es una estructura perteneciente a <pthread.h> la cual almacena la prioridad de cada Thread **/
-	int i, err;  														/** "i" se utiliza para el recorrido del ciclo for y "err" almacena codigo de error para pthread_create**/
+	int i, err;  														/** "i" se utiliza para el recorrido del ciclo for y "err" almacena codigo de error para pthread_create **/
 	
 	/** Inicializa todos los recursos usados por las Impresoras. **/
 	inicializar_Centro (); 												/** Se encarga de incializar los recurso del centro de impresion **/ 
@@ -79,14 +84,13 @@ int main(void) {
 	pthread_create( &Intro , NULL , Presentando , NULL ); 				/** Crea y lanza el Thread Intro con el procedimiento "Presentando" **/
 	
 	/************  CREACION DE HILOS USUARIO  **************/
-	for (i=0;i<N;i++) { 
+	for (i=0 ; i<N ; i++) {
 		pthread_attr_init(&atributos); 									/** Esta funcion inicializa el cuerpo de cada Thread con parametros por defecto **/
 		pthread_attr_setschedpolicy(&atributos, SCHED_FIFO); 			/** Modifica la politica de planificacion con FIFO **/
-		fifo_param.sched_priority = (rand() % 5+1); 					/** Genera un valor random entre 1-5 y se lo asigna a la variable sched_priority que esta en la estructura "shed_param" que tambien pertenece a <pthread.h>  **/
+		fifo_param.sched_priority = (rand() % 5+1); 					/** Genera un valor random entre 1-5 y se lo asigna a la variable sched_priority que esta en la estructura "shed_param" que pertenece a <pthread.h> **/
 		pthread_attr_setschedparam(&atributos, &fifo_param); 			/** Se almacena el valor "sched_priority" donde anteriormente se guardo el valor random, al cuerpo del hilo **/
 		
 		err = pthread_create( &Usuarios[i] , &atributos , funcion_Centro , NULL ); /** Crea el Thread, se le asignan los atributos, se almacena en el vector Usuarios, llama al procedimiento "funcion_Centro" sin parametros  **/
-		//err = pthread_create( &Usuarios[i] , NULL , funcion_Centro , NULL ); /** Crea el Thread, se le asignan los atributos, se almacena en el vector Usuarios, llama al procedimiento "funcion_Centro" sin parametros  **/
 		if (err != 0) {
 		   printf("Error creando el hilo %d\n", i);
 		   abort();
@@ -101,7 +105,7 @@ int main(void) {
 	return (0);
 }
 
-/** Procedimiento encargada de manejar la ejecucion de cada Thread **/
+/** Procedimiento encargado de manejar la ejecucion de cada Thread **/
 void *funcion_Centro () {
 	int id_Impresora; 
 	
@@ -119,47 +123,52 @@ void *funcion_Centro () {
 void inicializar_Centro () {
 	int i;
 	
-	for (i=0;i<M;i++) {
+	for (i=0 ; i<M ; i++) {
 		Centro[i].tinta = 100; 											/** Se le asigna el nivel de tinta = 100, es decir tanque lleno **/
 		Centro[i].identificador = i; 									/** Se le asigna el identificador a cada impresora (0,14) **/
+		Centro[i].uso = 0;												/** Se inicializa en 0 la cantidad de veces que se ha usado la impresora i **/
 		sem_init(&Centro[i].ocupada, 0, 1); 							/** Se inicializa el semaforo "ocupada" de cada impresora como libre **/
 	}
 	sem_init(&mutex_Centro, 0, 1); 										/** Semaforo que protege el acceso al arreglo Centro **/
 	sem_init(&mutex_impresoras_libres, 0, 1); 							/** Semaforo que protege las modificaciones a la cantidad de impresoras libres **/
 	Impresoras_libres = M; 												/** Cantidad de impresoras libres, inicialmente M **/
 	marca = N; 															/** Lleva la cuenta de cuantos usuarios hay en el Centro de Impresion **/
-	output = fopen("bitacora.out" , "w"); 								/** Se crea y se abre el Archivo donde se guardan los resultados en modo escritura **/
+	output = fopen("bitacora.out" , "w"); 								/** Se crea y se abre el Archivo de salida en modo escritura **/
 	
 	fprintf(output, "*** Centro de Impresion y Tecnico Listos para empezar ***\n\n");
 }
 
 /** Retorna el id de la PRIMERA impresora DISPONIBLE **/
 int requerir_impresora () {
-	int i = 0, id_impre, sval; 
-	bool band = true;
+	int i, id_impre, sval, j, menor = MAX;
 	
-	
-	pthread_mutex_lock(&mutex_requerido);
+	pthread_mutex_lock(&mutex_requerido);								/** Solo un Thread a la vez puede acceder a la seccion de requerir_impresora **/
 	while (Impresoras_libres < 1)
-		pthread_cond_wait(&cond_impresoras_libres,&mutex_requerido);
+		pthread_cond_wait(&cond_impresoras_libres,&mutex_requerido);	/** El semaforo "cond_impresoras_libres" se duerme mientras el semaforo "mutex_requerido" está bloqueado; mientras no hayan impresoras libres  **/
 	
-	pthread_mutex_unlock(&mutex_requerido);
-	pthread_cond_signal(&cond_impresoras_libres);
+	pthread_mutex_unlock(&mutex_requerido);								/** Desbloquea el semaforo "mutex_requerido" **/
+	pthread_cond_signal(&cond_impresoras_libres);						/** Despierta al semaforo "cond_impresoras_libres **/
 	
-	sem_wait(&mutex_Centro); 											/** Protege la Seccion Critica del arreglo Centro, de manera que solo un Thread este busnado una impresora disponible a la vez**/
-	while (band && (i >= 0) && (i < M)) { 
-		if ((sem_getvalue(&Centro[i].ocupada , &sval) == 0 ) && ( sval == 1 ) ) { /** Verifica si la impresora i esta disponible **/
-			band = false; 
-			id_impre = i;
-			sem_wait(&Centro[id_impre].ocupada); 						/** Bloquea el semaforo de la impresora i, poniendola como ocupada **/
-			
-			sem_wait(&mutex_impresoras_libres); 						/** Protege la modificacion de la cantidad de impresoras disponibles **/
-			Impresoras_libres--; 
-			sem_post(&mutex_impresoras_libres); 						/** Libera la modificacion de la cantidad de impresoras libres **/
+	sem_wait(&mutex_Centro); 											/** Protege la SC del arreglo Centro, de manera que solo un Thread este buscando una impresora disponible a la vez**/
+	for (i=0 ; i<M ; i++) {
+		if ((sem_getvalue(&Centro[i].ocupada , &sval) == 0 ) && ( sval == 1 ) ) {	 /** Verifica si la Impresora i esta disponible **/ 
+			if (Centro[i].uso <= menor) {								/** Verifica si es la Impresora i la menos usada **/
+				menor = Centro[i].uso;
+				j = i;
+			}
 		}
-		i++;
 	}
-	sem_post(&mutex_Centro); 											/** Libera la Seccion Critica del arreglo Centro **/
+	
+	id_impre = j;														/** Actualizo la variable de Retorno **/
+	sem_wait(&Centro[id_impre].ocupada); 								/** Bloquea el semaforo de la Impresora i, poniendola como ocupada **/
+		
+	sem_wait(&mutex_impresoras_libres); 								/** Protege el RC impresoras_libres para modificacion/lectura **/
+	Impresoras_libres--;
+	sem_post(&mutex_impresoras_libres); 								/** Libera el RC impresoras_libres **/
+	
+	Centro[id_impre].uso += 1;											/** Incremento en 1 la cantidad de veces que se ha usado la Impresora **/
+	
+	sem_post(&mutex_Centro); 											/** Libera la SC del arreglo Centro **/
 	
 	fprintf(output, "[Requerida] Impresora %d\n\n", id_impre);
 	
@@ -168,18 +177,14 @@ int requerir_impresora () {
 
 /** Libera una Impresora **/
 void liberar_impresora (int id_impresora) {
-	
-	sem_wait(&mutex_impresoras_libres);									/** Protege la modificacion de la cantidad de impresoras disponibles **/
-	Centro[id_impresora].tinta -= 5;
-	marca--;
+	sem_wait(&mutex_impresoras_libres);									/** Protege el RC impresoras_libres para modificacion/lectura **/
 	Impresoras_libres++;
-	sem_post(&mutex_impresoras_libres); 								/** Libera la modificacion de la cantidad de impresoras disponibles **/
+	sem_post(&mutex_impresoras_libres); 								/** Libera el RC impresoras_libres **/
 	
-	sem_post(&Centro[id_impresora].ocupada); 							/** Libera el semaforo de la impresora, poniendola como disponible **/
-	
-	
+	Centro[id_impresora].tinta -= 5;									/** Descuento 5ml de Tinta a la impresora que se esta liberando **/
+	marca--;
 	fprintf(output, "[Liberada] Impresora %d\n\n", id_impresora);
-	sleep(1);
+	sem_post(&Centro[id_impresora].ocupada); 							/** Libera el semaforo de la impresora, poniendola como disponible **/
 }
 
 bool verificar_tinta (int id_impresora) {
@@ -188,10 +193,10 @@ bool verificar_tinta (int id_impresora) {
 	fprintf(output, "[Verificando] Tinta Impresora %d\n", id_impresora);
 	if (Centro[id_impresora].tinta > 0) {
 		fprintf(output, "\tNivel de Tinta = %d \t[OK]\n\n", Centro[id_impresora].tinta);
-		band2 = true;
+		band2 = true;													/** Si la cantidad de tinta es mayor a cero retorna verdad**/
 	}else{
 		fprintf(output, "\tNivel de Tinta = %d \t[NO OK]\n\n", Centro[id_impresora].tinta);
-		band2 = false;
+		band2 = false;													/** Si la cantidad de tinta es menor a cero retorna falso**/
 	}
 	
 	return (band2);
@@ -202,7 +207,6 @@ void recargar_tinta (int id_impresora) {
 	
 	fprintf(output, "[Recargando] Tinta en Impresora: %d\n\t<Tecnico Trabajando> \t(...)\n\t¡Tinta Recargada.!\n\n", id_impresora);
 	Centro[id_impresora].tinta = 100; 									/** Tecnico restaura el nivel de tinta de la Impresora **/
-	sleep(2);
 	
 	pthread_mutex_unlock(&mutex_tecnico); 								/** Libera al Tecnico, poniendolo como DISPONIBLE **/
 }
@@ -256,7 +260,7 @@ void *Presentando () {
 	printf("\e[43;1m/**     Copyright © 2014-2015 SISTEMAS OPERATIVOS™, All Rights Reserved.    **/\e[m\n");
 	printf("\e[43;1m/*****************************************************************************/\e[m\n");
 	
-	sleep(5);
+	sleep(8);
 	system("reset");
 	
 	if (M == 15) {
